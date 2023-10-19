d76739f0f88fe75560d64f51c5a7f849da169b36
Nicole Torres <nicolejoan9@yahoo.com>
2023-10-19 15:19:13 -0400

    added more files to have log in data

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+const bcrypt = require ('bcrypt');
 
 class User extends Model {}
 
@@ -19,7 +20,7 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        len: [10],
+        len: [8],
       },
     },
   },
--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 require("dotenv").config();
 const express = require("express");
 const exphbs = require("express-handlebars");
+const routes = require("./controllers");
 const path = require("path");
-const hbs = exphbs.create({});
 const sequelize = require("./config/connection");
 
 
@@ -11,13 +11,14 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 // Set Handlebars as the default template engine.
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(require("./controllers/blog-routes"));
 
 // Starts the server to begin listening
 sequelize.sync({ force: false }).then(() => {
