1b101b978bf6deb511023865169ced0d6c85e1b5
Nicole Torres <nicolejoan9@yahoo.com>
2023-10-24 13:35:56 -0400

    Revert " got warning to successfully show in login page"
    This reverts commit c3147e1f22f68f38562e5ca1e18e1889be76d91d.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -84,7 +84,7 @@ router.get("/login", (req, res) => {
 
 router.get("/signup", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect("/profile");
+    res.redirect("/login");
     return;
   }
 
