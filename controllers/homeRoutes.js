c3147e1f22f68f38562e5ca1e18e1889be76d91d
Nicole Torres <nicolejoan9@yahoo.com>
2023-10-24 11:54:55 -0400

    got warning to successfully show in login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -84,7 +84,7 @@ router.get("/login", (req, res) => {
 
 router.get("/signup", (req, res) => {
   if (req.session.loggedIn) {
-    res.redirect("/login");
+    res.redirect("/profile");
     return;
   }
 
