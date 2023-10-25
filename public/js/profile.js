6b22d5412acd62312158de9e30685d15acfe916a
Nicole Torres <nicolejoan9@yahoo.com>
2023-10-25 18:12:04 -0400

    removed error message coming from profile.js by fixing pathing in fetch call

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,51 +1,3 @@
-// const newFormHandler = async (event) => {
-//   event.preventDefault();
-
-//   const post_name = document.querySelector("#blog-name").value.trim();
-//   const post_desc = document.querySelector("#blog-desc").value.trim();
- 
-
-//   if (post_name && post_desc) {
-//     const response = await fetch(`/api/users/blogs`, {
-//       method: 'POST',
-//       body: JSON.stringify({ post_name, post_desc}),
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/profile');
-//     } else {
-//       alert('Failed to create post');
-//     }
-//   }
-// };
-// ///////////////////////////////////////////
-// const delButtonHandler = async (event) => {
-//   if (event.target.hasAttribute('data-id')) {
-//     const id = event.target.getAttribute('data-id');
-
-//     const response = await fetch(`/api/projects/${id}`, {
-//       method: 'DELETE',
-//     });
-
-//     if (response.ok) {
-//       document.location.replace('/profile');
-//     } else {
-//       alert('Failed to delete project');
-//     }
-//   }
-// };
-
-// document
-//   .querySelector('.new-blog-form')
-//   .addEventListener('submit', newFormHandler);
-// /////////////////////////////////////////////////////////
-// document
-//   .querySelector('.project-list')
-//   .addEventListener('click', delButtonHandler);
-// /////////////////////////////////////////////////////////
 const newFormHandler = async (event) => {
   event.preventDefault();
 
@@ -54,7 +6,7 @@ const newFormHandler = async (event) => {
 
   if (post_name && post_desc) {
     try {
-    const response = await fetch("/api/users/blogs", {
+    const response = await fetch("/api/blogs/blogs", {
       method: "POST",
       body: JSON.stringify({ post_name, post_desc }),
       headers: {
@@ -76,4 +28,25 @@ const newFormHandler = async (event) => {
 const formElement = document.querySelector('.new-blog-form');
 if (formElement) {
   formElement.addEventListener('submit', newFormHandler);
+}
+
+const deleteBtn = async (event) => {
+  if (event.target.hasAttribute('data-id')) {
+    const id = event.target.getAttribute('data-id');
+
+    const response = await fetch(`/api/blogs/blogs/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (response.ok) {
+      document.location.replace('/profile');
+    } else {
+      alert('Failed to delete post');
+    }
+  }
+};
+
+const delElement = document.querySelector(".blog-list");
+if (delElement) {
+  delElement.addEventListener("click", deleteBtn);
 }
